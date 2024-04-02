c7508af2bcd5706f01c274a070a8d28cb72e93f0
sandorbence <szaniszlo47@gmail.com>
2024-04-02 11:19:54 +0200

    Randomize spawn times

--- project1/flappy/states/PlayState.lua
+++ project1/flappy/states/PlayState.lua
@@ -31,8 +31,8 @@ function PlayState:update(dt)
     -- update timer for pipe spawning
     self.timer = self.timer + dt
 
-    -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
+    -- spawn a new pipe pair at random times
+    if self.timer > math.random(2, 4) then
         -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
         -- no higher than 10 pixels below the top edge of the screen,
         -- and no lower than a gap length (90 pixels) from the bottom
