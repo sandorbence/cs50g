a915defdab99d6d81a0840b1fed380acbe25612b
sandorbence <szaniszlo47@gmail.com>
2024-04-02 10:50:23 +0200

    Randomize gap between pipes

--- project1/flappy/PipePair.lua
+++ project1/flappy/PipePair.lua
@@ -11,7 +11,7 @@
 PipePair = Class{}
 
 -- size of the gap between pipes
-local GAP_HEIGHT = 90
+local GAP_HEIGHT = math.random(80, 100)
 
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
